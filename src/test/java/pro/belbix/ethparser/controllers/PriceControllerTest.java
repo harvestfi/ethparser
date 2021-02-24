8f61a5685438c326fc3ba412c704c9588889c6a9
belbix <7453635@gmail.com>
2021-02-24 15:13:11 +0300

    polish price unit test

--- src/test/java/pro/belbix/ethparser/controllers/PriceControllerTest.java
+++ src/test/java/pro/belbix/ethparser/controllers/PriceControllerTest.java
@@ -1,7 +1,5 @@
 package pro.belbix.ethparser.controllers;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import static org.mockito.Mockito.doReturn;
 
 import java.net.URI;
@@ -15,33 +13,34 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.TimeZone;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Stream;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
-
 import pro.belbix.ethparser.Application;
 import pro.belbix.ethparser.entity.eth.TokenEntity;
 import pro.belbix.ethparser.model.RestResponse;
-import pro.belbix.ethparser.web3.contracts.ContractUtils;
 import pro.belbix.ethparser.web3.EthBlockService;
 import pro.belbix.ethparser.web3.contracts.ContractLoader;
+import pro.belbix.ethparser.web3.contracts.ContractUtils;
 
 @SpringBootTest(classes = Application.class)
 @ActiveProfiles("test")
 public class PriceControllerTest {
-    private final Double tolerancePct = 0.05;
-    private final long targetBlockNumber = 11790000L;
+
+    private static final Double TOLERANCE_PCT = 0.2;
+    private static final long TARGET_BLOCK_NUMBER = 11790000L;
 
     @SpyBean
     private EthBlockService ethBlockService;
@@ -52,17 +51,54 @@ public class PriceControllerTest {
     @Autowired
     private ContractLoader contractLoader;
 
-    private HashMap<String, Double> fetchPrices(String date) throws InterruptedException, ExecutionException, JSONException {
-        HashMap<String, Double> coinPriceMap = new HashMap<String, Double>();
+    private final Set<String> exclude = Set.of(
+        "ZERO",
+        "SBTC"
+    );
+
+    @TestFactory
+    public Stream<DynamicTest> tokenPrices() throws InterruptedException, ExecutionException, JSONException {
+        contractLoader.load();
+        doReturn(TARGET_BLOCK_NUMBER).when(ethBlockService).getLastBlock();
+        String dateStr = this.getDateByBlockNumber();
+        HashMap<String, Double> addressPriceMap = this.fetchPrices(dateStr);
+
+        return ContractUtils.getAllTokens().stream()
+            .filter(token -> !exclude.contains(token.getContract().getName()))
+            .map(token -> DynamicTest.dynamicTest("token: " + token.getSymbol(), () -> {
+                String tokenAddress = token.getContract().getAddress();
+                RestResponse response = priceController.token(tokenAddress);
+                if (!response.getCode().equals("200")) {
+                    Assertions.fail("Failed to calculate price for token: " + token.getSymbol());
+                }
+
+                String priceStr = response.getData();
+                double price = Double.parseDouble(priceStr.replace(",", "."));
+
+                Double coingeckoPrice = addressPriceMap.get(tokenAddress);
+                if (coingeckoPrice == null) {
+                    System.out.println("No external price found, Skip token test: " + token.getSymbol());
+                    return;
+                }
+
+                double priceToleranceDelta = coingeckoPrice * TOLERANCE_PCT;
+                Assertions.assertEquals(coingeckoPrice, price, priceToleranceDelta,
+                    "Token price deviation: " + token.getSymbol());
+            }));
+    }
+
+    private HashMap<String, Double> fetchPrices(String date)
+        throws InterruptedException, ExecutionException, JSONException {
+        HashMap<String, Double> coinPriceMap = new HashMap<>();
 
         URI coinListUri = getCoinListAPIUri();
         CompletableFuture<String> coinListFuture = this.callCoinGeckoAPI(coinListUri);
         String coinListJsonStr = coinListFuture.get();
         JSONArray coinListJsonArray = new JSONArray(coinListJsonStr);
 
-        List<CompletableFuture<Void>> futuresList = new ArrayList<CompletableFuture<Void>>();
-        
-        for (Integer i = 0; i < coinListJsonArray.length(); i++) {
+        List<CompletableFuture<Void>> futuresList = new ArrayList<>();
+
+        for (int i = 0; i < coinListJsonArray.length(); i++) {
             JSONObject coin = coinListJsonArray.getJSONObject(i);
             JSONObject platforms = coin.getJSONObject("platforms");
             if (!platforms.has("ethereum")) {
@@ -84,7 +120,7 @@ public class PriceControllerTest {
                     if (!coinHistoryJson.has("market_data")) {
                         return;
                     }
-    
+
                     String historyPrice = coinHistoryJson
                         .getJSONObject("market_data")
                         .getJSONObject("current_price")
@@ -100,7 +136,7 @@ public class PriceControllerTest {
         }
 
         CompletableFuture
-            .allOf(futuresList.toArray(new CompletableFuture[futuresList.size()]))
+            .allOf(futuresList.toArray(new CompletableFuture[0]))
             .get();
 
         return coinPriceMap;
@@ -125,45 +161,13 @@ public class PriceControllerTest {
             .thenApply(HttpResponse::body);
     }
 
-    private String getDateByBlockNumber(long blockNumber) {
-        long ts = ethBlockService.getTimestampSecForBlock(null, blockNumber);
-        
+    private String getDateByBlockNumber() {
+        long ts = ethBlockService.getTimestampSecForBlock(null, TARGET_BLOCK_NUMBER);
+
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
         dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-        String dateStr = dateFormat.format(new Date(ts * 1000));
-
-        return dateStr;
+        return dateFormat.format(new Date(ts * 1000));
     }
 
-    @TestFactory
-    public Stream<DynamicTest> tokenPrices() throws InterruptedException, ExecutionException, JSONException {
-        contractLoader.load();
-
-        doReturn(this.targetBlockNumber).when(ethBlockService).getLastBlock();
-        String dateStr = this.getDateByBlockNumber(this.targetBlockNumber);
-        HashMap<String, Double> addressPriceMap = this.fetchPrices(dateStr);
-
-        return ContractUtils.getAllTokens().stream()
-            .filter(token -> token.getSymbol() != "ZERO" && !token.getSymbol().isEmpty())
-            .map(token -> DynamicTest.dynamicTest("token: " + token.getSymbol(), () -> {
-                String tokenAddress = token.getContract().getAddress();
-                RestResponse response = priceController.token(tokenAddress);
-                if (response.getCode() != "200") {
-                    fail("Failed to calculate price for token: " + token.getSymbol());
-                }
-
-                String priceStr = response.getData();
-                Double price = Double.parseDouble(priceStr.replace(",", "."));
-
-                Double coingeckoPrice = addressPriceMap.get(tokenAddress);
-                if (coingeckoPrice == null) {
-                    System.out.println("No external price found, Skip token test: " + token.getSymbol());
-                    return;
-                }
-
-                Double priceToleranceDelta = coingeckoPrice * this.tolerancePct;
-                assertEquals("Token price deviation: " + token.getSymbol(), coingeckoPrice, price, priceToleranceDelta);
-            }));
-    }
 
 }
