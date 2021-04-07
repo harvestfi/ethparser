c4394c4111785eae2e6f0707a2194f84f43bb8e8
belbix <7453635@gmail.com>
2021-04-07 13:52:43 +0300

    fix ws cors8

--- src/main/java/pro/belbix/ethparser/CorsFilter.java
+++ src/main/java/pro/belbix/ethparser/CorsFilter.java
@@ -17,14 +17,14 @@ public class CorsFilter extends OncePerRequestFilter {
       @NotNull final HttpServletRequest request,
       final HttpServletResponse response,
       final FilterChain filterChain) throws ServletException, IOException {
-    response.addHeader("Access-Control-Allow-Origin", "*");
+    response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
     response.addHeader("Access-Control-Allow-Methods",
         "GET, POST, DELETE, PUT, PATCH, HEAD");
     response.addHeader("Access-Control-Allow-Headers",
         "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     response.addHeader("Access-Control-Expose-Headers",
         "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
-    response.addHeader("Access-Control-Allow-Credentials", "false");
+    response.addHeader("Access-Control-Allow-Credentials", "true");
     response.addIntHeader("Access-Control-Max-Age", 3600);
     filterChain.doFilter(request, response);
   }
