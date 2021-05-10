6130f3580de3593612bf66e150180f6dc4d1c190
belbix <7453635@gmail.com>
2021-05-10 11:51:40 +0300

    add pages endpoints for harvest and uni

--- src/main/java/pro/belbix/ethparser/controllers/HarvestController.java
+++ src/main/java/pro/belbix/ethparser/controllers/HarvestController.java
@@ -4,14 +4,22 @@ import static pro.belbix.ethparser.service.AbiProviderService.ETH_NETWORK;
 import static pro.belbix.ethparser.utils.CommonUtils.parseLong;
 
 import java.util.List;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.web3j.protocol.ObjectMapperFactory;
 import pro.belbix.ethparser.dto.v0.HarvestDTO;
+import pro.belbix.ethparser.model.PaginatedResponse;
+import pro.belbix.ethparser.model.RestResponse;
 import pro.belbix.ethparser.repositories.v0.HarvestRepository;
 import pro.belbix.ethparser.repositories.v0.HarvestRepository.UserBalance;
 import pro.belbix.ethparser.web3.contracts.ContractType;
@@ -20,6 +28,7 @@ import pro.belbix.ethparser.web3.harvest.db.VaultActionsDBService;
 
 @ConditionalOnExpression("!${ethparser.onlyParse:false}")
 @RestController
+@Log4j2
 public class HarvestController {
 
     private final HarvestRepository harvestRepository;
@@ -81,6 +90,62 @@ public class HarvestController {
         @RequestParam(value = "network", required = false, defaultValue = ETH_NETWORK) String network
     ) {
         return harvestRepository.fetchOwnerBalances(network);
+    }
+
+    @GetMapping(value = "/harvest/pages")
+    public RestResponse harvestPages(
+        @RequestParam("pageSize") String pageSize,
+        @RequestParam("page") String page,
+        @RequestParam(value = "ordering", required = false) String ordering,
+        @RequestParam(value = "vault", required = false) String vault,
+        @RequestParam(value = "minAmount", required = false) Integer minAmount,
+        @RequestParam(value = "network", required = false, defaultValue = ETH_NETWORK) String network
+    ) {
+        try {
+            int start = Integer.parseInt(page);
+            int size = Integer.parseInt(pageSize);
+            Sort sorting = Sort.by("blockDate");
+            if (!Strings.isBlank(ordering) && "desc".equals(ordering)) {
+                sorting = sorting.descending();
+            }
+
+            Page<HarvestDTO> pages;
+            if (minAmount == null) {
+                minAmount = Integer.MIN_VALUE;
+            }
+            if (Strings.isBlank(vault)) {
+                pages = harvestRepository
+                    .fetchPages(minAmount, network, PageRequest.of(start, size, sorting));
+            } else {
+                if (!vault.startsWith("0x")) {
+                    vault = contractDbService.getAddressByName(vault, ContractType.VAULT, network)
+                        .orElseThrow();
+                }
+                pages = harvestRepository
+                    .fetchPagesByVault(vault, network, minAmount,
+                        PageRequest.of(start, size, sorting));
+            }
+
+            if (!pages.hasContent()) {
+                return RestResponse.error("Data not found");
+            }
+            return RestResponse.ok(
+                ObjectMapperFactory.getObjectMapper().writeValueAsString(
+                    PaginatedResponse.builder()
+                        .currentPage(start)
+                        .previousPage(pages.hasPrevious() ? start - 1 : -1)
+                        .nextPage(pages.hasNext() ? start + 1 : -1)
+                        .totalPages(pages.getTotalPages())
+                        .data(pages.getContent())
+                        .build()
+                )
+            );
+
+        } catch (Exception e) {
+            String msg = "Error get hardwork pages";
+            log.warn(msg, e);
+            return RestResponse.error(msg);
+        }
     }
 
 }
--- src/main/java/pro/belbix/ethparser/controllers/UniController.java
+++ src/main/java/pro/belbix/ethparser/controllers/UniController.java
@@ -4,14 +4,22 @@ import static pro.belbix.ethparser.service.AbiProviderService.ETH_NETWORK;
 import static pro.belbix.ethparser.utils.CommonUtils.parseLong;
 
 import java.util.List;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.web3j.protocol.ObjectMapperFactory;
 import pro.belbix.ethparser.dto.v0.UniswapDTO;
+import pro.belbix.ethparser.model.PaginatedResponse;
+import pro.belbix.ethparser.model.RestResponse;
 import pro.belbix.ethparser.repositories.v0.UniswapRepository;
 import pro.belbix.ethparser.repositories.v0.UniswapRepository.OhlcProjection;
 import pro.belbix.ethparser.web3.contracts.ContractType;
@@ -20,6 +28,7 @@ import pro.belbix.ethparser.web3.uniswap.db.UniswapDbService;
 
 @ConditionalOnExpression("!${ethparser.onlyParse:false}")
 @RestController
+@Log4j2
 public class UniController {
 
     private final UniswapRepository uniswapRepository;
@@ -59,6 +68,62 @@ public class UniController {
                                               @RequestParam(value = "to", required = false) String to) {
         return uniswapRepository.fetchAllByOwner(
             address.toLowerCase(), parseLong(from, 0), parseLong(to, Long.MAX_VALUE));
+    }
+
+    @GetMapping(value = "/uni/pages")
+    public RestResponse uniPages(
+        @RequestParam("pageSize") String pageSize,
+        @RequestParam("page") String page,
+        @RequestParam(value = "ordering", required = false) String ordering,
+        @RequestParam(value = "token", required = false) String token,
+        @RequestParam(value = "minAmount", required = false) Integer minAmount,
+        @RequestParam(value = "network", required = false, defaultValue = ETH_NETWORK) String network
+    ) {
+        try {
+            int start = Integer.parseInt(page);
+            int size = Integer.parseInt(pageSize);
+            Sort sorting = Sort.by("blockDate");
+            if (!Strings.isBlank(ordering) && "desc".equals(ordering)) {
+                sorting = sorting.descending();
+            }
+
+            Page<UniswapDTO> pages;
+            if (minAmount == null) {
+                minAmount = Integer.MIN_VALUE;
+            }
+            if (Strings.isBlank(token)) {
+                pages = uniswapRepository
+                    .fetchPages(minAmount, PageRequest.of(start, size, sorting));
+            } else {
+                if (!token.startsWith("0x")) {
+                    token = contractDbService.getAddressByName(token, ContractType.TOKEN, network)
+                        .orElseThrow();
+                }
+                pages = uniswapRepository
+                    .fetchPagesByToken(token, minAmount,
+                        PageRequest.of(start, size, sorting));
+            }
+
+            if (!pages.hasContent()) {
+                return RestResponse.error("Data not found");
+            }
+            return RestResponse.ok(
+                ObjectMapperFactory.getObjectMapper().writeValueAsString(
+                    PaginatedResponse.builder()
+                        .currentPage(start)
+                        .previousPage(pages.hasPrevious() ? start - 1 : -1)
+                        .nextPage(pages.hasNext() ? start + 1 : -1)
+                        .totalPages(pages.getTotalPages())
+                        .data(pages.getContent())
+                        .build()
+                )
+            );
+
+        } catch (Exception e) {
+            String msg = "Error get hardwork pages";
+            log.warn(msg, e);
+            return RestResponse.error(msg);
+        }
     }
 
 }
