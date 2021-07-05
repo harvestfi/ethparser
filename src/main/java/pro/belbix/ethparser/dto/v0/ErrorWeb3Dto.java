000e5cdd273fa8abb3ce023a17fae78e4d5cbd1d
yagosupro <yagosupro@gmail.com>
2021-07-05 19:13:36 +0300

    If we cannot parse something and catch error, we put in base table(error_parse). Added a service for handling errors that occurred during parsing(ErrorService)(fixedRate=3h)

--- src/main/java/pro/belbix/ethparser/dto/v0/ErrorWeb3Dto.java
+++ src/main/java/pro/belbix/ethparser/dto/v0/ErrorWeb3Dto.java
@@ -1,26 +1,28 @@
 package pro.belbix.ethparser.dto.v0;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.web3j.protocol.core.methods.response.Transaction;
 
 @Entity
-@Table(name="error_parse")
+@Table(name = "error_parse")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class ErrorWeb3Dto {
+
   @Id
-  private String id;
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  @Lob
+  private String json;
+  private String errorClass;
   private String network;
-  private String address;
-  private String transactionHash;
-//  public Transaction findTransaction(String hash, String network)
-
-
 }
--- src/main/java/pro/belbix/ethparser/web3/harvest/parser/ImportantEventsParser.java
+++ src/main/java/pro/belbix/ethparser/web3/harvest/parser/ImportantEventsParser.java
@@ -15,6 +15,7 @@ import pro.belbix.ethparser.model.ImportantEventsInfo;
 import pro.belbix.ethparser.model.tx.ImportantEventsTx;
 import pro.belbix.ethparser.properties.AppProperties;
 import pro.belbix.ethparser.properties.NetworkProperties;
+import pro.belbix.ethparser.repositories.ErrorsRepository;
 import pro.belbix.ethparser.web3.EthBlockService;
 import pro.belbix.ethparser.web3.ParserInfo;
 import pro.belbix.ethparser.web3.Web3Parser;
@@ -45,8 +46,9 @@ public class ImportantEventsParser extends Web3Parser<ImportantEventsDTO, Log> {
       EthBlockService ethBlockService,
       FunctionsUtils functionsUtils, AppProperties appProperties,
       NetworkProperties networkProperties,
-      ContractDbService contractDbService) {
-    super(parserInfo, appProperties);
+      ContractDbService contractDbService,
+      ErrorsRepository errorsRepository) {
+    super(parserInfo, appProperties, errorsRepository);
     this.web3Subscriber = web3Subscriber;
     this.importantEventsDbService = importantEventsDbService;
     this.ethBlockService = ethBlockService;
