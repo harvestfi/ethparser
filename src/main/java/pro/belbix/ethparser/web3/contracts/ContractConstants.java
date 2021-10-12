3160cb1ed7ddbc74f19d37da182d1aac0cb69ddb
alex <alex.b@lineup.com.ua>
2021-10-13 00:54:32 +0300

    add matic constants to ONE_DOLLAR_TOKENS

--- src/main/java/pro/belbix/ethparser/web3/contracts/ContractConstants.java
+++ src/main/java/pro/belbix/ethparser/web3/contracts/ContractConstants.java
@@ -136,7 +136,9 @@ public class ContractConstants {
   public static final Set<String> ONE_DOLLAR_TOKENS = Set.of(
       "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48".toLowerCase(), //USDC
       "0xe9e7cea3dedca5984780bafc599bd69add087d56".toLowerCase(), //BUSD
-      "0xdAC17F958D2ee523a2206206994597C13D831ec7".toLowerCase() //USDT
+      "0xdAC17F958D2ee523a2206206994597C13D831ec7".toLowerCase(), //USDT
+      "0x2791bca1f2de4661ed88a30c99a7a9449aa84174".toLowerCase(), //matic USDC
+      "0xc2132d05d31c914a87c6611c10748aeb04b58e8f".toLowerCase() //matic USDT
   );
 
   //Key tokens are used to find liquidity for any given token on Uni, Sushi and Curve.
