1abde35931be2c9b161ef8665931f0dd0d43c8ee
alex <alexandersazonov0@gmail.com>
2022-04-26 23:27:45 +0300

    Try to resolve problem with NoClassDefFoundError

--- scripts/run_app/run.sh
+++ scripts/run_app/run.sh
@@ -1,7 +1,7 @@
 rm -rf ./lib
 rm -rf ./logs
 rm ethparser.jar
-mvn install -Dmaven.test.skip=true -f ./../../pom.xml
+mvn install -Dmaven.test.skip=true -Dmaven.version=1.0.1 -f ./../../pom.xml
 cp -R ./../../dist/lib/. ./lib
 cp ./../../dist/ethparser.jar ./ethparser.jar
 java -Xmx4g -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Dspring.config.location=./../application.yml,run_config.yml -cp ethparser.jar pro.belbix.ethparser.Application
